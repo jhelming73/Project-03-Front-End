b770a22332256b872dd198d9d27c9a615c5fff52
Jared Helming <helmingjared@johndeere.com>
2020-03-24 11:54:37 -0500

    Initial Revision:
    1. Created baseline class component with test message

---
+++ src/components/Product/Product.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Product extends Component {
+    constructor () {
+        super ()
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <h1>Inside of Product</h1>
+        )
+    }
+}
+
+export default Product
