513f6008003f2b9c9c814afb5466c8587f3035bb
Jared Helming <helmingjared@johndeere.com>
2020-03-24 11:49:01 -0500

    Mulitple Updates:
    1. Imported Admin and AddProduct components
    2. Created className for nav tag for .css changes
    3. Added three routes to /, /Admin, /AddProduct

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
 import { Route, Link, Redirect, Switch, withRouter} from "react-router-dom";
 import './App.css';
+import Admin from '../Admin/Admin';
+import AddProduct from '../AddProduct/AddProduct';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-
+     
     }
   }
 
   render() {
     return (
       <div className="App">
-        <nav>
+        <nav className="AppNav">
           <div>
-          <h1>Jared &amp; Seamus's Grubhub</h1>
+          <h1>Jared &amp; Seamus' Grubhub</h1>
           <Link to="/Admin">Admin</Link>
           </div>
           <hr />
         </nav>
+        <Switch>
+            <Route exact path ="/" />
+            <Route path="/Admin" render={() => (<Admin {...this.state} />) } />
+            <Route path="/AddProduct" render={(props) => (<AddProduct {...props} />)} />
+        </Switch>
         </div>
     );
   }
