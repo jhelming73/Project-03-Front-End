ff8c97d5ec7cacf8e13d3331d17ff49d2022862c
Jared Helming <helmingjared@johndeere.com>
2020-03-24 16:13:11 -0500

    Multiple Updates:
    1. Imported Product
    2. Creatd mapped array of Product components (lines 9-11)
    3. Updated the link from AddProduct to ManageProduct
    4. Changed h1 tag from Admin main page to Manage Products
    5. Rendered array of Product components

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import "./Admin.css";
+import Product from '../Product/Product';
 
 const Admin = (props) => {
-    console.log("Inside Admin.return", props)
+    console.log("Inside Admin props=", props)
+
+    let products = props.products.map(product => {
+        return <Product key={product._id} product={product} />
+    })
+
     return (
         <div className="Admin">
-            <h1>Adminstration main page</h1>
+            <h1>Adminstration Products Page</h1>
             <nav>
-                <Link to='/AddProduct'>Add Product</Link>
+                <Link to='/ManageProduct'>Add Product</Link>
                 <Link to='/'>Home</Link>
             </nav>
+            <div className="ProductsContainer">
+                {products}
+            </div>
         </div>
     )
 }
