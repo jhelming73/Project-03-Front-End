8a4dad5b60922b57755f696cb8cc95aabac4be5a
Jared Helming <helmingjared@johndeere.com>
2020-03-24 16:23:05 -0500

    Mulitple Updates:
    1. Converted from class to functional component
    2. Rendered image, description and price
    3. Created a footer for links for update Product and Delete

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,18 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import "./Product.css"
 
-class Product extends Component {
-    constructor () {
-        super ()
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return (
-            <h1>Inside of Product</h1>
-        )
-    }
-}
+const Product = (props) => {
+    console.log("Inside Product props=", props)
+           return (
+            <div className="Product">
+               <img src={props.product.imageURL}></img>
+               <div>     
+                    <label className="Description">{props.product.description}</label>
+                    <label className="Price">${props.product.price.$numberDecimal}</label>
+                    <footer>
+                    <Link to='/ManageProduct'>Update</Link> 
+                    <Link to=''>Delete</Link>
+                    </footer>
+               </div>
+            </div>
+       )
+   }
 
 export default Product
