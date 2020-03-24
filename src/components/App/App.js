0072b2c4c60408430fca6496beef10c6250c8c19
Jared Helming <helmingjared@johndeere.com>
2020-03-24 16:16:19 -0500

    Multiple Updates:
    1. Changed AddProduct to ManageProduct
    2. Imported axios
    3. Created backend URL variable for api endpoint
    4. Added products and orders array to state
    5. Created a componentDidMount that calls getProduct and getOrders
    6. Created getProducts that sets the products state array from axios api endpoint call
    7. Created getOrders that sets the orders state array from axios api endpoint call
    8. Updated Admin route with props
    9. Updated AddProduct route to ManageProduct route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,15 +2,50 @@ import React, { Component } from 'react';
 import { Route, Link, Redirect, Switch, withRouter} from "react-router-dom";
 import './App.css';
 import Admin from '../Admin/Admin';
-import AddProduct from '../AddProduct/AddProduct';
+import ManageProduct from '../ManageProduct/ManageProduct';
+import axios from 'axios';
+
+const backendURL = "http://localhost:8080/api";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-     
+     products: [],
+     orders: []
     }
+  }
+
+  componentDidMount(){
+    this.getProducts()
+    this.getOrders()
+  }
+
+  getProducts() {
+    axios({ 
+      method: 'get',
+      url: `${backendURL}/products`
+    })
+    .then(products => {
+      this.setState({
+        products: products.data
+      })
+      console.log("Inside App.getProducts.then()", this.state)
+    })
+  }
+
+  getOrders() {
+    axios({ 
+      method: 'get',
+      url: `${backendURL}/orders`
+    })
+    .then(orders => {
+      this.setState({
+        orders: orders.data
+      })
+      console.log("Inside App.getOrders.then()", this.state)
+    })
   }
 
   render() {
@@ -25,8 +60,8 @@ class App extends Component {
         </nav>
         <Switch>
             <Route exact path ="/" />
-            <Route path="/Admin" render={() => (<Admin {...this.state} />) } />
-            <Route path="/AddProduct" render={(props) => (<AddProduct {...props} />)} />
+            <Route path="/Admin" render={(props) => (<Admin {...props} {...this.state} />) } />
+            <Route path="/ManageProduct" render={(props) => (<ManageProduct {...props} />)} />
         </Switch>
         </div>
     );
