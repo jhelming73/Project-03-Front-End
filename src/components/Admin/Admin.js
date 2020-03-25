4a2df3bbd35291588dbf6b29b1713aa1db06e819
Seamus Murphy <seamusm02@gmail.com>
2020-03-25 05:54:37 -0500

    Multiple Updates
    1. Updated h1 tag to h2 tag with slightly different text
    2. Removed Link to '/' Home
    3. Added optional semi-colons at end of statements

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -4,24 +4,23 @@ import "./Admin.css";
 import Product from '../Product/Product';
 
 const Admin = (props) => {
-    console.log("Inside Admin props=", props)
+   console.log("Inside Admin, props =", props);
 
     let products = props.products.map(product => {
-        return <Product key={product._id} product={product} />
-    })
+        return <Product key={product._id} product={product} />;
+    });
 
     return (
         <div className="Admin">
-            <h1>Adminstration Products Page</h1>
+            <h2>Adminstration - Products</h2>
             <nav>
                 <Link to='/ManageProduct'>Add Product</Link>
-                <Link to='/'>Home</Link>
             </nav>
             <div className="ProductsContainer">
                 {products}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Admin
+export default Admin;
