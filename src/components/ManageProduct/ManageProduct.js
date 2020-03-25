2cc4f8e654cfa51f3931080ed9c602b78de4ea63
Seamus Murphy <seamusm02@gmail.com>
2020-03-25 06:05:10 -0500

    Multiple Updates
    1. Added componentDidMount() which calls handleLinkUpdate
    2. Updated className from 'AddProduct' to 'ManageProduct'
    3. Updated h1 tag to h2 tag with slightly different text
    4. Added alt='' to img tag to avoid warnings
    5. Added / Updated console.log statements
    6. Added optional semi-colons at end of statements

--- src/components/ManageProduct/ManageProduct.js
+++ src/components/ManageProduct/ManageProduct.js
@@ -1,52 +1,58 @@
 import React, { Component } from 'react';
-import "./ManageProduct.css"
+import "./ManageProduct.css";
 import axios from 'axios';
 
 const backendUrl = "http://localhost:8080/api/products/";
 
 class ManageProduct extends Component {
+    
     constructor(props) {        
-        super(props)
+        super(props);
+        
         this.state = {
             description: "",
             imageURL: "",
             price: 0.0
-        }
-        console.log("Inside ManageProduct.contructor()", this.props)
+        };
+        console.log("Inside ManageProduct.contructor(), props =", this.props, "state =", this.state);
     }
     
+    componentDidMount() {
+        console.log("Inside ManageProduct.componentDidMount(), props =", this.props, "state =", this.state);
+        this.props.handleLinkUpdate();
+      }
+   
     handleChange = (event) => {
         this.setState({
           [event.target.name]: event.target.value
-        })
-        console.log("Inside ManageProduct.handleChange()", this.state)
+        });
+        console.log("Inside ManageProduct.handleChange(), props =", this.props, "state =", this.state);
       }
 
       handleSubmit = event => {
-        event.preventDefault()
-        this.createProduct()
-        this.props.history.push('/Admin')
+        event.preventDefault();
+        this.createProduct();
+        this.props.history.push('/Admin');
       }
 
       createProduct() {
         axios({
-          method: "POST",
+          method: "post",
           url: backendUrl,
           data: {
             description: this.state.description,
             imageURL: this.state.imageURL,
             price: this.state.price
           }
-        })  .then(product => {
-            console.log(product)
-        }) 
+        }).then(product => {
+            console.log(product);
+        });
     }
-
 
     render() {
         return (
-            <div className="AddProduct">
-                <h1>Adminstration Add Product page</h1>
+            <div className="ManageProduct">
+                <h2>Adminstration - Add Product</h2>
                 <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
                     <div>
                         <label>Description:</label>
@@ -56,7 +62,7 @@ class ManageProduct extends Component {
                     <div>
                         <label>Image URL:</label>
                         <input type="text" name="imageURL" />
-                        <img src={this.state.imageURL}></img>
+                        <img src={this.state.imageURL} alt=""></img>
                     </div>
                     <div>
                         <label>Price:</label>
@@ -65,8 +71,8 @@ class ManageProduct extends Component {
                     <input type="submit" />
                 </form>
             </div>
-        )
+        );
     }
 }
 
-export default ManageProduct
+export default ManageProduct;
