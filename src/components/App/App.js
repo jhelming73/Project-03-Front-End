593206d1e02a9c59586ac41192b1e2954cfcc51b
Seamus Murphy <seamusm02@gmail.com>
2020-03-25 05:55:04 -0500

    Multiple Updates
    1.  Removed {withRouter} from import - no idea what that's for
    2.  Added props to constructor
    3.  Added state properties productAction and navLink
    4.  Added componentDidUpdate() - no functionality yet
    5.  Added / updated console.log statments
    6.  Added handleLinkUpdate() to toggle nav link between Home and Admin
    7.  Replaced nav tag in render() with header tag
    8.  Updated Link in header to dynamically switch between Home and Admin/Admin
    9.  Updated ManageProduct Route with handleLinkUpdate prop
    10. Added Redirect Route to '/' for all non-specific Routes
    11. Added optional semi-colons at end of statements

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link, Redirect, Switch, withRouter} from "react-router-dom";
+import { Route, Link, Redirect, Switch} from "react-router-dom";
 import './App.css';
 import Admin from '../Admin/Admin';
 import ManageProduct from '../ManageProduct/ManageProduct';
@@ -8,18 +8,39 @@ import axios from 'axios';
 const backendURL = "http://localhost:8080/api";
 
 class App extends Component {
-  constructor() {
-    super();
+
+  constructor(props) {
+    super(props);
 
     this.state = {
+     productAction: '',
+     navLink: 'Admin',
      products: [],
      orders: []
-    }
+    };
+    console.log("Inside App.constructor(), props =", this.props, "state =", this.state);
   }
 
-  componentDidMount(){
-    this.getProducts()
-    this.getOrders()
+  componentDidMount() {
+    this.getProducts();
+    this.getOrders();
+    console.log("Inside App.componentDidMount(), props =", this.props, "state =", this.state);
+  }
+
+  componentDidUpdate() {
+    console.log("Inside App.componentDidUpdate(), props =", this.props, "state =", this.state);
+  }
+
+  handleLinkUpdate = () => {
+    console.log("Inside App.handleLinkUpdate(), props =", this.props, "state =", this.state);
+    
+    switch(this.state.navLink) {
+      case 'Admin': this.setState({ navLink: 'Home'}); break;
+      case 'Home' : this.setState({ navLink: 'Admin'}); break;
+      default: this.setState({ navLink: 'Admin'});
+    }
+
+  //  this.setState({ navLink: link });
   }
 
   getProducts() {
@@ -30,9 +51,9 @@ class App extends Component {
     .then(products => {
       this.setState({
         products: products.data
-      })
-      console.log("Inside App.getProducts.then()", this.state)
-    })
+      });
+      console.log("Inside App.getProducts.axios.then(), props =", this.props, "state =", this.state);
+    });
   }
 
   getOrders() {
@@ -43,25 +64,28 @@ class App extends Component {
     .then(orders => {
       this.setState({
         orders: orders.data
-      })
-      console.log("Inside App.getOrders.then()", this.state)
-    })
+      });
+      console.log("Inside App.getOrders.axios.then(), props =", this.props, "state =", this.state);
+    });
   }
 
   render() {
+    console.log("Inside App.render(), props =", this.props, "state =", this.state);
     return (
       <div className="App">
-        <nav className="AppNav">
+        <header>
           <div>
-          <h1>Jared &amp; Seamus' Grubhub</h1>
-          <Link to="/Admin">Admin</Link>
+            <h1>Jared &amp; Seamus' Grubhub</h1>
+            <Link to={`/${this.state.navLink}`} id='App' onClick={this.handleLinkUpdate}>{this.state.navLink}</Link>
           </div>
           <hr />
-        </nav>
+        </header>
         <Switch>
             <Route exact path ="/" />
-            <Route path="/Admin" render={(props) => (<Admin {...props} {...this.state} />) } />
-            <Route path="/ManageProduct" render={(props) => (<ManageProduct {...props} />)} />
+            <Route path="/Admin" render={(props) => <Admin {...props} {...this.state} />} />
+            <Route path="/ManageProduct" render={ (props) => <ManageProduct {...props} 
+              handleLinkUpdate={this.handleLinkUpdate} />} />
+            <Route path="/*" render={() => <Redirect to="/" />} />
         </Switch>
         </div>
     );
