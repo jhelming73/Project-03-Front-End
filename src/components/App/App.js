de2af30cf53b086d42fd1f79e993e7447b906a81
Seamus Murphy <seamusm02@gmail.com>
2020-03-26 09:36:57 -0500

    Multiple Updates
    1.  Added withRouter back to enable proper page rendering
    2.  Replaced Admin component with Products component
    3.  Moved backendURL inside constructor
    4.  State now holds three arrays:
          a. dbProducts
          b. dbOrders
          c. currentOrder - Holds application order before db submission
    5.  Commented code for getOrders since we aren't using Orders from db yet
    6.  Put code in componentDidUpdate to redirect to '/' when user removes all products from order
    7.  Removed handleLinkUpdate()
    8.  Added handler and function for adding a product to an order
    9.  Added handler and function for removing a product from an order
    10. Added handler and function for submitting an order - submitOrder is just a stub now
    11. Added create product handler (not sure where the function is now ;-)
    12. Minor updates to getProducts function
    13. Minor updates to getOrders function
    14. Updated Link in header from goofy dynamic one to '/Products'
    15. Updated Route to '/' with props
    16. Removed Route to '/Admin'
    17. Removed handleLinkUpdate from '/ManageProduct' Route
    18. Added Route to '/order-details'
    19. Added Route to '/submit-order'
    20. Added withRouter wrapper around App in export statement

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,56 +1,108 @@
 import React, { Component } from 'react';
-import { Route, Link, Redirect, Switch} from "react-router-dom";
+import { Route, Link, Redirect, Switch, withRouter} from "react-router-dom";
 import './App.css';
-import Admin from '../Admin/Admin';
+import Products from '../Products/Products';
 import ManageProduct from '../ManageProduct/ManageProduct';
 import axios from 'axios';
-
-const backendURL = "http://localhost:8080/api";
 
 class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-     productAction: '',
-     navLink: 'Admin',
-     products: [],
-     orders: []
+    this.backendURL = "http://localhost:8080/api";
+
+    this.state = {        
+        dbProducts:   []
+      , dbOrders:     []
+      , currentOrder: []      
     };
     console.log("Inside App.constructor(), props =", this.props, "state =", this.state);
-  }
+    this.removeProductFromOrder = this.removeProductFromOrder.bind(this);
+ }
 
   componentDidMount() {
     this.getProducts();
-    this.getOrders();
+    // this.getOrders(); => We want to create an order not get them 
     console.log("Inside App.componentDidMount(), props =", this.props, "state =", this.state);
   }
 
   componentDidUpdate() {
     console.log("Inside App.componentDidUpdate(), props =", this.props, "state =", this.state);
-  }
 
-  handleLinkUpdate = () => {
-    console.log("Inside App.handleLinkUpdate(), props =", this.props, "state =", this.state);
-    
-    switch(this.state.navLink) {
-      case 'Admin': this.setState({ navLink: 'Home'}); break;
-      case 'Home' : this.setState({ navLink: 'Admin'}); break;
-      default: this.setState({ navLink: 'Admin'});
+    if (this.props.location.pathname === '/order-details' && this.state.currentOrder.length === 0) {
+      this.props.history.push('/');  // Redirect to '/' 
     }
-
-  //  this.setState({ navLink: link });
+  }
+ 
+  handleAddProductToOrder = (product) => {
+    console.log("Inside App.handleAddProductToOrder(), product =", product);
+    this.addProductToOrder(product);  
   }
 
+  addProductToOrder = (product) => {
+    console.log("Inside App.addProductToOrder(), product =", product);
+    this.setState(({
+      currentOrder: [...this.state.currentOrder, product]
+    }));
+  }
+
+  handleRemoveProductFromOrder = (event, product) => {
+    event.preventDefault();
+    console.log("Inside App.handleRemoveProductFromOrder(), product =", product);    
+    this.removeProductFromOrder(product); 
+  }
+
+  removeProductFromOrder = (product) => {    
+    console.log("Inside App.removeProductFromOrder(), product =", product);
+    
+    let arrCpy = this.state.currentOrder.slice();
+    let index = null;
+
+    for (let i = 0; i < arrCpy.length; i++){
+      console.log("arrCpy[i]._id = ", arrCpy[i]._id, " product._id = ", product._id); 
+      if (arrCpy[i]._id == product._id) {
+        index = i;
+        break;
+      };
+    }    
+
+    console.log("index = ", index); 
+    arrCpy.splice(index, 1);
+    console.log("arrCpy after splice = ", arrCpy);
+    console.log("this.state.currentOrder = ", this.state.currentOrder);
+    
+    if (index !== null) {
+      this.setState(({
+        currentOrder: arrCpy
+      }));
+    }
+ }
+
+ handleSubmitOrder = (event, currentOrder) => {
+  event.preventDefault();
+  console.log("Inside App.handleSubmitOrder(), currentOrder =", currentOrder);    
+  this.submitOrder(currentOrder); 
+}
+
+submitOrder = (currentOrder) => {
+  console.log("Inside App.submitOrder(), currentOrder =", currentOrder);   
+}
+
+ handleCreateProduct = event => {
+    event.preventDefault();
+    this.createProduct();
+    this.props.history.push('/Admin');
+  }
+  
   getProducts() {
     axios({ 
       method: 'get',
-      url: `${backendURL}/products`
+      url: `${this.backendURL}/products`
     })
-    .then(products => {
+    .then(dbProducts => {
       this.setState({
-        products: products.data
+        dbProducts: dbProducts.data
       });
       console.log("Inside App.getProducts.axios.then(), props =", this.props, "state =", this.state);
     });
@@ -59,11 +111,11 @@ class App extends Component {
   getOrders() {
     axios({ 
       method: 'get',
-      url: `${backendURL}/orders`
+      url: `${this.backendURL}/orders`
     })
-    .then(orders => {
+    .then(dbOrders => {
       this.setState({
-        orders: orders.data
+        dbOrders: dbOrders.data
       });
       console.log("Inside App.getOrders.axios.then(), props =", this.props, "state =", this.state);
     });
@@ -76,20 +128,30 @@ class App extends Component {
         <header>
           <div>
             <h1>Jared &amp; Seamus' Grubhub</h1>
-            <Link to={`/${this.state.navLink}`} id='App' onClick={this.handleLinkUpdate}>{this.state.navLink}</Link>
+            <Link to='/Products' onClick={this.handleLinkUpdate}>Header Link Text</Link>
           </div>
-          <hr />
+          <hr />          
         </header>
         <Switch>
-            <Route exact path ="/" />
-            <Route path="/Admin" render={(props) => <Admin {...props} {...this.state} />} />
-            <Route path="/ManageProduct" render={ (props) => <ManageProduct {...props} 
-              handleLinkUpdate={this.handleLinkUpdate} />} />
+            
+            <Route exact path ="/" render={(props) => <Products {...props} {...this.state} 
+              handleAddProductToOrder={this.handleAddProductToOrder} />} />
+            
+            <Route path="/ManageProduct" render={ (props) => <ManageProduct {...props} {...this.state}
+              backendURL={this.backendURL} />} />
+
+            <Route path="/order-details" render={ (props) => <Products {...props} {...this.state}
+               handleRemoveProductFromOrder={this.handleRemoveProductFromOrder} />} />
+
+            <Route path="/submit-order" render={ (props) => <Products {...props} {...this.state}
+               handleSubmitOrder={this.handleSubmitOrder} />} />
+            
             <Route path="/*" render={() => <Redirect to="/" />} />
+
         </Switch>
         </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
