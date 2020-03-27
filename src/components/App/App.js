be16766882156b3e097264eba6085069379b2592
Seamus Murphy <seamusm02@gmail.com>
2020-03-27 14:42:00 -0500

    ?

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.backendURL = "http://localhost:8080/api";
+    this.backendURL = process.env.REACT_APP_BACKEND_APP_URL || "http://localhost:8080/api";
 
     this.state = {        
         dbProducts:   []
